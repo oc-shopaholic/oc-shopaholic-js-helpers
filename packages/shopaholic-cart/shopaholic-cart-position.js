4627d86ae7bcb6595d987324c6d84975e579b3f5
Andrey Kharanenka <a.khoronenko@lovata.com>
2019-04-04 10:30:48 +0300

    Updated shopaholic-cart package

---
+++ packages/shopaholic-cart/shopaholic-cart-position.js
@@ -0,0 +1,121 @@
+/**
+ * @author  Andrey Kharanenka, <a.khoronenko@lovata.com>, LOVATA Group,
+ * @author  Uladzimir Ambrazhey, <v.ambrazhey@lovata.com>, LOVATA Group
+ */
+export default class ShopaholicCartPosition {
+  constructor(obButton) {
+    this.sDefaultWrapperClass = '_shopaholic-product-wrapper';
+    this.sWrapperSelector = `.${this.sDefaultWrapperClass}`;
+
+    this.sPositionIDAttr = 'data-position-id';
+    this.sOfferIDAttr = 'offer_id';
+    this.sQuantityAttr = 'quantity';
+
+    this.obButton = obButton;
+    this.obProductCart = obButton.closest(`${this.sWrapperSelector}`);
+
+    this.iPositionID = null;
+    this.iOfferID = null;
+    this.iQuantity = 1;
+    this.iRadix = 10;
+
+    if (!this.obProductCart) {
+      throw new Error('Product wrapper is empty. It mast contain product card node');
+    }
+
+    this.initOfferID();
+    this.initQuantity();
+    this.initCartPositionID();
+  }
+
+  /**
+   * Get cart position node
+   * @returns {*}
+   */
+  getNode() {
+    return this.obProductCart;
+  }
+
+  /**
+   * Get cart position data 
+   * @returns {{quantity: number, id: null, offer_id: null}}
+   */
+  getData() {
+    let obData = {
+      'id': this.iPositionID,
+      'offer_id': this.iOfferID,
+      'quantity': this.iQuantity,
+    };
+
+    return obData;
+  }
+
+  /**
+   * Get position ID
+   * @returns {int}
+   */
+  getPositionID() {
+    return this.iPositionID;
+  }
+
+  /**
+   * Get quantity
+   * @returns {int}
+   */
+  getQuantity() {
+    return this.iQuantity;
+  }
+
+  /**
+   * Get offeer ID from input
+   */
+  initOfferID() {
+    const obOfferIDNodeCollection = this.obProductCart.querySelectorAll(`[name=${this.sOfferIDAttr}]`);
+    if (!obOfferIDNodeCollection || obOfferIDNodeCollection.length === 0) {
+      return;
+    }
+
+    const isRadio = this.getOfferIDInputType(obOfferIDNodeCollection);
+    if (isRadio) {
+      const obOfferIDNode = [...obOfferIDNodeCollection].filter(node => node.checked);
+
+      this.iOfferID = parseInt(obOfferIDNode[0].value, this.iRadix);
+    } else {
+      this.iOfferID = parseInt(obOfferIDNodeCollection[0].value, this.iRadix);
+    }
+  }
+
+  /**
+   * Detect type of input with offer id
+   */
+  getOfferIDInputType(obOfferIDNodeCollection) {
+    const firstNode = obOfferIDNodeCollection[0];
+    const {type: sType} = firstNode;
+
+    return sType === 'radio';
+  }
+
+  /**
+   * Get offer quantity from cart object
+   */
+  initQuantity() {
+    const obQuantityInput = this.obProductCart.querySelector(`[name=${this.sQuantityAttr}]`);
+    if (!obQuantityInput) {
+      return;
+    }
+
+    this.iQuantity = parseInt(obQuantityInput.value, this.iRadix);
+  }
+
+  /**
+   * Get offer quantity from cart object
+   */
+  initCartPositionID() {
+    const obAttribute = this.obProductCart.getAttributeNode(`${this.sPositionIDAttr}`);
+    if (!obAttribute) {
+      return;
+    }
+
+    this.iPositionID = parseInt(obAttribute.value, this.iRadix);
+  }
+}
